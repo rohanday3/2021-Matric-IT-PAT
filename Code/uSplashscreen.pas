a92c0fabf028d29f9f37c7ae4de5f1372cd63eaf
Rohan Dayaram <rohanday4@gmail.com>
2021-08-22 19:34:09 +0200

    'Restructure'

---
+++ Code/uSplashscreen.pas
@@ -0,0 +1,80 @@
+﻿{ ******************************************************************************
+  ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤIT MATRIC PAT 2021 - Robin Hood
+  ㅤㅤㅤㅤㅤCopyright (C) 2021 by Rohan Dayaram <rohanday4@gmail.com>
+  ㅤㅤㅤㅤㅤㅤㅤhttps://github.com/rohanday3/2021-Matric-IT-PAT
+  ******************************************************************************
+  Robin Hood is a multi-purpose company management system for the financial and
+  inventory systems etc of the Robin Hood© foundation which is for the purposes
+  of my matric pat, an NGO dedicated to feeding homeless children under the ages
+  of 3.  The  company needed a system to manage their inventory,  payments  and
+  other systems which would otherwise be managed by staff and recorded on paper.
+  The design is a modern one with a  dashboard and a multiview drawer  which is
+  used to navigate the other tabs.  Many additional "nice to have" features are
+  included as well which are docuemented in the help tab as well as the feature
+  docuement included in the same folder as the project and can also be found on
+  my github page stated above.
+  ****************************************************************************** }
+
+unit uSplashscreen;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  uDonationsThread, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects;
+
+type
+  TSplashscreen = class(TForm)
+    ProgressBar1: TProgressBar;
+    Timer1: TTimer;
+    Image1: TImage;
+    procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private declarations }
+    procedure onDonationsLoaded(Sender: TObject);
+  public
+    { Public declarations }
+    class function Execute: boolean;
+  end;
+
+var
+  Splashscreen: TSplashscreen;
+  donation_thread: TLoadDonations;
+
+implementation
+
+{$R *.fmx}
+
+procedure TSplashscreen.FormCreate(Sender: TObject);
+begin
+  donation_thread := TLoadDonations.Create();
+  donation_thread.OnTerminate := onDonationsLoaded;
+end;
+
+procedure TSplashscreen.onDonationsLoaded(Sender: TObject);
+begin
+  ModalResult := mrOk;
+end;
+
+procedure TSplashscreen.Timer1Timer(Sender: TObject);
+begin
+  ProgressBar1.Value := ProgressBar1.Value + 1;
+  if ProgressBar1.Value = ProgressBar1.Max then
+    Timer1.Enabled := False;
+end;
+
+class function TSplashscreen.Execute: boolean;
+begin
+  with TSplashscreen.Create(nil) do
+    try
+      Result := ShowModal = mrOk;
+    finally
+      Free;
+      donation_thread.Free;
+    end;
+end;
+
+end.
--- Feature test/PasswordStrength/Unit4.pas
+++ Feature test/PasswordStrength/Unit4.pas
@@ -35,6 +35,13 @@ type
     imgCreateBtnBack: TImage;
     lblCreateBtnBack: TLabel;
     lblCreateAccountPasswordError: TLabel;
+    rectPassStrength: TRectangle;
+    rectPassStrength1: TRectangle;
+    rectPassStrength2: TRectangle;
+    rectPassStrength5: TRectangle;
+    rectPassStrength4: TRectangle;
+    rectPassStrength3: TRectangle;
+    lblPassStrength: TLabel;
     procedure rectCreateShowPasswordClick(Sender: TObject);
     procedure rectCreateShowPasswordMouseEnter(Sender: TObject);
     procedure rectCreateShowPasswordMouseLeave(Sender: TObject);
@@ -44,6 +51,7 @@ type
     procedure password_strength(Password: string);
     function StrInArray(const Value: String;
       const ArrayOfString: Array of String): Boolean;
+    procedure passStrengthMeter(strength: integer);
   public
     { Public declarations }
   end;
@@ -59,26 +67,104 @@ procedure TForm4.edtCreateAccountPasswordTyping(Sender: TObject);
 var
   frmPass: TForm1;
 begin
-  frmPass := TForm1.Create(self);
-  frmPass.Position := TFormPosition.Designed;
-  frmPass.Left := round(rectCreateAccountPassword.AbsoluteToLocal(edtCreateAccountPassword.Position.Point).X);
-  frmPass.Top := round(rectCreateAccountPassword.AbsoluteToLocal(edtCreateAccountPassword.Position.Point).Y);
-  frmPass.Show;                                                        
+  {
+    frmPass := TForm1.Create(self);
+    frmPass.Position := TFormPosition.Designed;
+    frmPass.Left := round(rectCreateAccountPassword.AbsoluteToLocal(edtCreateAccountPassword.Position.Point).X);
+    frmPass.Top := round(rectCreateAccountPassword.AbsoluteToLocal(edtCreateAccountPassword.Position.Point).Y);
+    frmPass.Show;
+  }
+  password_strength(edtCreateAccountPassword.Text);
+end;
+
+procedure TForm4.passStrengthMeter(strength: integer);
+const
+  captions: array of string = ['Too short', 'Weak', 'Poor', 'Fair', 'Good',
+    'Strong'];
+var
+  grey, red, orange, yellow, blue, green: TAlphaColor;
+
+begin
+  grey := StringToAlphaColor('#FFE0E0E0');
+  red := StringToAlphaColor('#FFE44242');
+  orange := StringToAlphaColor('#FFF99F4A');
+  yellow := StringToAlphaColor('#FFf3CC3E');
+  blue := StringToAlphaColor('#FF1493FF');
+  green := StringToAlphaColor('#FF36C565');
+  lblPassStrength.Text := 'Password strength: ' + captions[strength];
+  case strength of
+    0:
+      begin
+        rectPassStrength1.Fill.Color := grey;
+        rectPassStrength2.Fill.Color := grey;
+        rectPassStrength3.Fill.Color := grey;
+        rectPassStrength4.Fill.Color := grey;
+        rectPassStrength5.Fill.Color := grey;
+      end;
+    1:
+      begin
+        rectPassStrength1.Fill.Color := red;
+        rectPassStrength2.Fill.Color := grey;
+        rectPassStrength3.Fill.Color := grey;
+        rectPassStrength4.Fill.Color := grey;
+        rectPassStrength5.Fill.Color := grey;
+      end;
+    2:
+      begin
+        rectPassStrength1.Fill.Color := orange;
+        rectPassStrength2.Fill.Color := orange;
+        rectPassStrength3.Fill.Color := grey;
+        rectPassStrength4.Fill.Color := grey;
+        rectPassStrength5.Fill.Color := grey;
+      end;
+    3:
+      begin
+        rectPassStrength1.Fill.Color := yellow;
+        rectPassStrength2.Fill.Color := yellow;
+        rectPassStrength3.Fill.Color := yellow;
+        rectPassStrength4.Fill.Color := grey;
+        rectPassStrength5.Fill.Color := grey;
+      end;
+    4:
+      begin
+        rectPassStrength1.Fill.Color := blue;
+        rectPassStrength2.Fill.Color := blue;
+        rectPassStrength3.Fill.Color := blue;
+        rectPassStrength4.Fill.Color := blue;
+        rectPassStrength5.Fill.Color := grey;
+      end;
+    5:
+      begin
+        rectPassStrength1.Fill.Color := green;
+        rectPassStrength2.Fill.Color := green;
+        rectPassStrength3.Fill.Color := green;
+        rectPassStrength4.Fill.Color := green;
+        rectPassStrength5.Fill.Color := green;
+      end;
+  end;
 end;
 
 procedure TForm4.password_strength(Password: string);
 var
-  Len, number, upp, low, special: Boolean;
+  Len, number, upp, low, special, req: Boolean;
   s: char;
   k: char;
+  n: char;
   points: integer;
+  j: TObject;
 const
-  numbers = [0 .. 9];
+  numbers = ['0' .. '9'];
   special_char = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
     '_', '-', '+', '=', '{', '}', '[', ']', '|', '\', ':', ';', '"', '''', '>',
     '<', '.', ',', '?', '/'];
 
 begin
+
+  Len := False;
+  number := False;
+  upp := False;
+  low := False;
+  special := False;
   // Verify the strength of 'password'
   // Returns a dict indicating the wrong criteria
   // A password is considered strong if:
@@ -93,67 +179,71 @@ begin
   // Numbers: 0-9
   // Symbols: ~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/
 
+  points := 0;
+
   if (Length(Password) >= 8) then
     Len := True;
 
   for s in Password do
   begin
-    try
-      if strtoint(s) in numbers then
-        number := True;
-    except
-
+    if s in numbers then
+    begin
+      number := True;
+      // Break
     end;
     if s = UpperCase(s) then
       upp := True;
     if s = Lowercase(s) then
       low := True;
-    for k in special_char do
-      if s = k then
-        special := True;
+    if s in special_char then
+    begin
+      special := True;
+      // Break;
+    end;
   end;
 
-  if upp then
-    Inc(points, 20);
-
-  if low then
-    Inc(points, 20);
-
-  if special then
-    Inc(points, 20);
-
   if Len then
-    Inc(points, 20);
-
+    Inc(points);
   if number then
-    Inc(points, 20);
+    Inc(points);
+  if upp then
+    Inc(points);
+  if low then
+    Inc(points);
+  if special then
+    Inc(points);
+
+  // ShowMessage('Length: ' + BoolToStr(Len) + #13 + 'Number: ' + BoolToStr(number)
+  // + #13 + 'Uppercasse: ' + BoolToStr(upp) + #13 + 'Lowercase: ' + BoolToStr(low) + #13 + 'Special: ' + BoolToStr(special) + #13 + 'Score: ' + IntToStr(points));
+
+  passStrengthMeter(points);
 
 end;
 
 procedure TForm4.rectCreateShowPasswordClick(Sender: TObject);
 begin
-  if rectCreateShowPasswordBox.Fill.Color = StringToAlphacolor('#ffffffff') then
+  if rectCreateShowPasswordBox.Fill.Color = StringToAlphaColor('#ffffffff') then
   begin
-    rectCreateShowPasswordBox.Fill.Color := StringToAlphacolor('#ff135fc9');
+    rectCreateShowPasswordBox.Fill.Color := StringToAlphaColor('#ff135fc9');
     edtCreateAccountPassword.Password := False;
   end
   else
   begin
-    rectCreateShowPasswordBox.Fill.Color := StringToAlphacolor('#ffffffff');
+    rectCreateShowPasswordBox.Fill.Color := StringToAlphaColor('#ffffffff');
     edtCreateAccountPassword.Password := True;
   end;
 end;
 
 procedure TForm4.rectCreateShowPasswordMouseEnter(Sender: TObject);
 begin
-  if rectCreateShowPasswordBox.Fill.Color = StringToAlphacolor('#ff1a79ff') then
-    rectCreateShowPasswordBox.Fill.Color := StringToAlphacolor('#ff135fc9');
+  if rectCreateShowPasswordBox.Fill.Color = StringToAlphaColor('#ff1a79ff') then
+    rectCreateShowPasswordBox.Fill.Color := StringToAlphaColor('#ff135fc9');
 end;
 
 procedure TForm4.rectCreateShowPasswordMouseLeave(Sender: TObject);
 begin
-  if rectCreateShowPasswordBox.Fill.Color = StringToAlphacolor('#ff135fc9') then
-    rectCreateShowPasswordBox.Fill.Color := StringToAlphacolor('#ff1a79ff');
+  if rectCreateShowPasswordBox.Fill.Color = StringToAlphaColor('#ff135fc9') then
+    rectCreateShowPasswordBox.Fill.Color := StringToAlphaColor('#ff1a79ff');
 end;
 
 function TForm4.StrInArray(const Value: String;
